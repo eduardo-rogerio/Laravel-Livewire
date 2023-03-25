d8a64707a28f0f30265c52e36e368f95b8d3ed63
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-03-25 18:39:18 -0300

    feat() 13

--- app/Http/Livewire/Expense/ExpenseCreate.php
+++ app/Http/Livewire/Expense/ExpenseCreate.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Livewire\Expense;
 
+use App\Models\Expenses;
 use Livewire\Component;
 
 class ExpenseCreate extends Component
 {
+    public $amount;
+    public $description;
+    public $type;
     public function render()
     {
         return view('livewire.expense.expense-create');
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'amount' => 'required',
+            'description' => 'required',
+            'type' => 'required',
+        ]);
+
+        $expense = new Expenses();
+        $expense->amount = $this->amount;
+        $expense->description = $this->description;
+        $expense->type = $this->type;
+        $expense->save();
+
+        $this->resetInputFields();
+        $this->emit('expenseAdded');
     }
 }
--- resources/views/livewire/expense/expense-create.blade.php
+++ resources/views/livewire/expense/expense-create.blade.php
@@ -1,3 +1,24 @@
 <div>
-  <p>Expense Create</p>
+    <h3>Criar Registro</h3>
+    <hr>
+    <form action="">
+        <p>
+            <label>Descrição Registro</label>
+            <input type="text" wire:model="description" class="shadow border-t">
+        </p>
+
+        <p>
+            <label>Valor do Registro</label>
+            <input type="text" wire:model="amount" class="shadow border-t">
+        </p>
+
+        <p>
+            <label>Tipo do Registro</label>
+            <select name="type" id="" class="shadow border-t" wire:model="type">
+                <option value="1">Entrada</option>
+                <option value="2">Saída</option>
+            </select>
+        </p>
+        <button type="submit" wire:click.prevent="store()">Criar Registro</button>
+    </form>
 </div>
