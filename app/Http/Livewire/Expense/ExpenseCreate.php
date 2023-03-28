22b8fb48e5b4da3d6e63d2868db27b4d1e681e16
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-03-27 22:28:40 -0300

    feat() aula 25

--- app/Http/Livewire/Expense/ExpenseCreate.php
+++ app/Http/Livewire/Expense/ExpenseCreate.php
@@ -4,12 +4,16 @@ namespace App\Http\Livewire\Expense;
 
 use App\Models\Expenses;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ExpenseCreate extends Component
 {
+    use WithFileUploads;
+
     public $amount;
     public $description;
     public $type;
+    public $photo;
 
     public function render()
     {
@@ -20,17 +24,22 @@ class ExpenseCreate extends Component
         'amount' => 'required|numeric',
         'description' => 'required',
         'type' => 'required',
+        'photo' => 'image|max:4098', // 4MB Max
     ];
 
     public function createExpense()
     {
         $this->validate();
 
+        if ($this->photo) {
+            $photo = $this->photo->store('expenses-photos', 'public');
+        }
         Expenses::create([
             'amount' => $this->amount,
             'description' => $this->description,
             'type' => $this->type,
             'user_id' => '1',
+            'photo' => $photo ?? null,
         ]);
 
         session()->flash('message', 'Expense created successfully.');
