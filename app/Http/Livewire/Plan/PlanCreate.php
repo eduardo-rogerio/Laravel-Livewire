403d6165f2e423a0ca29c4231c639b2cfec33549
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-03-29 01:23:44 -0300

    feat() aula 34

--- app/Http/Livewire/Plan/PlanCreate.php
+++ app/Http/Livewire/Plan/PlanCreate.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Livewire\Plan;
 
+use App\Models\Plan;
 use Livewire\Component;
 
 class PlanCreate extends Component
 {
+    public array $plan = [];
+
+    protected $rules = [
+        'plan.name' => 'required',
+        'plan.price' => 'required',
+        'plan.slug' => 'required',
+        'plan.description' => 'required',
+        'plan.reference' => 'required',
+    ];
+
+    public function createPlan()
+    {
+        $this->validate();
+        $plan = $this->plan;
+        $plan['reference'] = 'PagSeguro';
+
+        Plan::create($plan);
+
+        session()->flash('message', 'Plano criado com sucesso.');
+    }
+
     public function render()
     {
         return view('livewire.plan.plan-create');
